d5ee55c3c64eba345380ba0b65ed863dcc3006d7
rocky123454321 <rockyjayducusin3@gmail.com>
2025-12-17 14:48:58 +0800

    Add new code / update messages

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -4,7 +4,7 @@ const MessageBox = ({ text }) => {
   return (
     <div className="message-box">
       <div className="message-text">{text}</div>
-      <input type="text" placeholder="Send message to admin" />
+
     </div>
   );
 };
